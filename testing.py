ceaca3d7e0aa9c5ae969f7a642f3f24bd7fef74b
Aviral <avimittal30@gmail.com>
2024-03-09 15:55:27 +0530

    added common.py and init.py file

---
+++ testing.py
@@ -0,0 +1,19 @@
+d={'a':'value1', 'b':'value2'}
+
+d['a']
+
+d.a
+
+# helps us access value using '.' instead of d['a']
+from box import ConfigBox
+d=ConfigBox({'a':'value1', 'b':'value2'})
+d.a
+
+from ensure import ensure_annotations
+
+@ensure_annotations
+def get_product(x:int, y:int)->int:
+    return x*y
+# Without ensure_annotations, this will not through any error and will give wierd output
+get_product(2,"3")
+
